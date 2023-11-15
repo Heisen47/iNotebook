9f7196b988f02d6e156c913bfd3cef1b171c27ec
Heisen47 <89003879+Heisen47@users.noreply.github.com>
2023-11-15 10:28:39 +0530

    connected to the db, added data validator package in auth.js

--- iNotebook/backend/index.js
+++ iNotebook/backend/index.js
@@ -1,15 +1,17 @@
-const connectToMongo = require ('./db')
-const express = require('express')
+const connectToMongo = require('./db')
+const express = require("express");
 
-connectToMongo()
+
+connectToMongo();
 
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(express.json())
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
